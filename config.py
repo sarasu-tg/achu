92f6213189441885607f728267cba42c65f40aff
mntgx <mntgxo0@gmail.com>
2024-12-24 13:04:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "2b653cb53821a82097efaba6732f5d75")
     
     # Fixing BOT_TOKENS extraction from environment
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7633441501:AAG_9lTk_vnTtraLFIMkJGKNV88yFMNfwPU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8066778521:AAEc92bY8EfSOoBEG94sZMjPf5aQHybEAaw")
 
     # Database config
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
